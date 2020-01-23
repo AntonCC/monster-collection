36e489d966c7f54bb964fdde63b46b104236d773
Anton Chetvertkov <antonchetvertkov@Antons-MacBook-Pro.local>
2020-01-23 17:36:02 -0500

    Updates

---
+++ precache-manifest.289a25be684eb7d609421210bc2d762d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78cc4a595243b7a84a08442e74678000",
+    "url": "/index.html"
+  },
+  {
+    "revision": "360f206c69ae5c173340",
+    "url": "/static/css/main.6725b23a.chunk.css"
+  },
+  {
+    "revision": "44bb97ba1760b10e12fa",
+    "url": "/static/js/2.ee37e390.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ee37e390.chunk.js.LICENSE"
+  },
+  {
+    "revision": "360f206c69ae5c173340",
+    "url": "/static/js/main.e8219cb6.chunk.js"
+  },
+  {
+    "revision": "0078db31cc403a99b201",
+    "url": "/static/js/runtime-main.06a30682.js"
+  }
+]);
